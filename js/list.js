57b968dd0aa64040456a00db7950a5ec0cc9c091
songys <yellowsys92@naver.com>
2020-12-22 13:49:08 +0900

    성공

--- js/list.js
+++ js/list.js
@@ -2,9 +2,10 @@
 function myFunc() {
     document.write("<ul><li>Apple</li><li>Banana</li><li>Strawberry</li></ul>");
     let dan = 4;
-    document.write('<tr>');
+    document.write('<table border="1" style = "width:50">');
     for (let i =1; i<=10; i++) {
-            document.write('<td>' +dan + '*'+ i +'</td>'+'<td>'+'=' +'</td>'+ '<td><b>'+(dan*i)+'</b></td>' +'</td>');
+            document.write('<tr><td>' +dan + '*'+ i +'</td>'+'<td>'+'=' +'</td>'+ '<td>'+(dan*i)+'</td>' +'</td></tr>');
     }
-    document.write('</tr>')
+    document.write('</table>');
+
 }
